a90a43f876c511309d517162346a2c1b25b1ad3b
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-12 14:53:36 -0600

    Add methods for iteration 1,2

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,18 @@
 class Classroom
+  attr_reader :capacity, :subject, :students
+  def initialize(subject, capacity)
+    @capacity = capacity
+    @subject = subject
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students
+    @students.map do |uppercase|
+      uppercase.upcase
+    end
+  end
 end
